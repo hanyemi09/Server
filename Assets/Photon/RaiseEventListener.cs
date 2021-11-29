c90ed9cc522c00eb9c97b89963a0d8231d60cdfa
hanyemi09 <54048632+hanyemi09@users.noreply.github.com>
2021-11-30 01:36:57 +0800

    Server

---
+++ Assets/Photon/RaiseEventListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ExitGames.Client.Photon;
+using Photon.Pun;
+
+public class RaiseEventListener : MonoBehaviourPun
+{
+    private void OnEnable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived += OnEvent;
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived += OnEvent;
+    }
+
+    private void OnEvent(EventData obj)
+    {
+        Debug.Log(obj);
+        Debug.Log("Receiving Evnet");
+        Debug.Log("OnEvent : " + (string)obj.CustomData);
+    }
+}
---
+++ Assets/Photon/SendMessage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+public class SendMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSendMessageButtonClicked()
+    {
+        Debug.Log("Checking connection! Trying to send message!");
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.IsConnected)
+        {
+            Debug.Log("Connected! Sending Message!");
+            string message = "Hello World";
+            byte[] content = Encoding.Default.GetBytes(message);
+            PhotonNetwork.RaiseEvent(1, content, RaiseEventOptions.Default, SendOptions.SendReliable);
+        }
+    }
+}
